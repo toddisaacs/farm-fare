15ab5d3dce564fcb675292e00093b346599d78c1
Todd Isaacs <teisaacs21w@gmail.com>
2017-10-17 10:06:17 -0400

    Added bling js

---
+++ public/js/modules/bling.js
@@ -0,0 +1,19 @@
+/* bling.js */
+//https://gist.github.com/paulirish/12fb951a8b893a454b32
+
+const $ = document.querySelectorAll.bind(document);
+const $$ = document.querySelector.bind(document);
+
+Node.prototype.on = window.on = function (name, fn) {
+  this.addEventListener(name, fn);
+}
+
+NodeList.prototype.__proto__ = Array.prototype;
+
+NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn) {
+  this.forEach(function (elem, i) {
+    elem.on(name, fn);
+  });
+}
+
+export { $, $$ };
