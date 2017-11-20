fd8360f3540dd5b0c5cf9ccff471abb9b187b9bc
Todd Isaacs <teisaacs21w@gmail.com>
2017-11-20 13:40:15 -0500

    added autocomplete to address field and autopopulated lat & lng

--- public/js/farm-fare.js
+++ public/js/farm-fare.js
@@ -1,7 +1,10 @@
 import '../sass/style.scss';
 import { $, $$ } from './modules/bling';
 import { makeMap, getLocation } from './modules/map';
+import autocomplete from './modules/autocomplete';
 
 const map = $('#map');
+
+autocomplete($('#address'), $('#lat'), $('#lng'));
 
 makeMap(map);
---
+++ public/js/modules/autocomplete.js
@@ -0,0 +1,23 @@
+function autocomplete(input, lat, lng) {
+  if (!input) return;
+
+  const dropdown = new google.maps.places.Autocomplete(input);
+  
+  //listen and react to changes
+  dropdown.addListener('place_changed', () => {
+    const place = dropdown.getPlace();
+
+    if(place.geometry) {
+      lat.value = place.geometry.location.lat();
+      lng.value = place.geometry.location.lng();
+    }
+  });
+  
+  input.on('keydown', (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+    }
+  })
+}
+
+export default autocomplete;
