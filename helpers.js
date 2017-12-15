e5abe30998469154f87e8dc1276ac2d8c2f865b6
Todd Isaacs <teisaacs21w@gmail.com>
2017-12-15 08:00:11 -0500

    added details page with static map

--- helpers.js
+++ helpers.js
@@ -3,3 +3,7 @@ const fs = require('fs');
 exports.icon = (name) => {
   return fs.readFileSync(`./public/images/icons/${name}`);
 }
+
+exports.staticMap = ([lat, lng]) => {
+  return `https://maps.googleapis.com/maps/api/staticmap?center=${lng},${lat}&zoom=12&size=800x200&key=${process.env.MAP_KEY_VEGGIE_STAND}&markers=${lng},${lat}&scale=1`;
+}
